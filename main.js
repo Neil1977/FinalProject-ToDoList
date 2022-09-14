476014398f86c8621fe7c4c5e1d5b5bad98b3e88
Neil1977 <Neil_lunn1977@hotmail.com>
2022-09-14 16:00:26 +0100

    date/time order added

--- main.js
+++ main.js
@@ -44,100 +44,84 @@ function DisplayTodos() {
     const todoList = document.querySelector('#todo-list');
 
     todoList.innerHTML = '';
-    //ORDER TODOS INTO DATE/TIME? - INSERT INTO EXISTING CODE HOW?
 
-    //todos.sort(createdAt(a,b).forEach(todo => {
-    //return new Date(b.date) - new Date(a.date);
-    //});
+    todos.sort((a, b) => b.createdAt - a.createdAt)
+        .forEach(todo => {
+            const todoItem = document.createElement('div');
+            todoItem.classList.add('todo-item')
 
-    //OR...?
+            const label = document.createElement('label');
+            const input = document.createElement('input');
+            const span = document.createElement('span');
+            const content = document.createElement('div');
+            const actions = document.createElement('div');
+            const edit = document.createElement('button');
+            const deleteButton = document.createElement('button');
 
-    //const sortedTodos = todos.slice().sort((a,b) => b.date - a.date)
+            input.type = 'checkbox';
+            input.checked = todo.done;
+            span.classList.add('bubble');
 
-    //OR...?
+            if (todo.category == 'personal') {
+                span.classList.add('personal');
+            } else {
+                span.classList.add('business');
+            }
 
-    //const todoList = todos.sort((a,b) => {
-    //const aDate = new Date(a.date + ' ' + a.time)
-    //const bDate = new Date(b.date + ' ' + b.time)
-    //returnbDate.getTime() - aDate.getTime()
-    //})
+            content.classList.add('todo-content');
+            actions.classList.add('actions');
+            edit.classList.add('edit');
+            deleteButton.classList.add('delete');
 
-    todos.forEach(todo => {
-        const todoItem = document.createElement('div');
-        todoItem.classList.add('todo-item')
+            content.innerHTML = ` <input type="text" value="${todo.content}" readonly>`;
+            edit.innerHTML = 'Edit';
+            deleteButton.innerHTML = 'Delete';
 
-        const label = document.createElement('label');
-        const input = document.createElement('input');
-        const span = document.createElement('span');
-        const content = document.createElement('div');
-        const actions = document.createElement('div');
-        const edit = document.createElement('button');
-        const deleteButton = document.createElement('button');
+            label.appendChild(input);
+            label.appendChild(span);
+            actions.appendChild(edit);
+            actions.appendChild(deleteButton);
+            todoItem.appendChild(label);
+            todoItem.appendChild(content);
+            todoItem.appendChild(actions);
 
-        input.type = 'checkbox';
-        input.checked = todo.done;
-        span.classList.add('bubble');
-
-        if (todo.category == 'personal') {
-            span.classList.add('personal');
-        } else {
-            span.classList.add('business');
-        }
-
-        content.classList.add('todo-content');
-        actions.classList.add('actions');
-        edit.classList.add('edit');
-        deleteButton.classList.add('delete');
-
-        content.innerHTML = ` <input type="text" value="${todo.content}" readonly>`;
-        edit.innerHTML = 'Edit';
-        deleteButton.innerHTML = 'Delete';
-
-        label.appendChild(input);
-        label.appendChild(span);
-        actions.appendChild(edit);
-        actions.appendChild(deleteButton);
-        todoItem.appendChild(label);
-        todoItem.appendChild(content);
-        todoItem.appendChild(actions);
-
-        todoList.appendChild(todoItem);
-
-        if (todo.done) {
-            todoItem.classList.add('done');
-        }
-
-        input.addEventListener('click', e => {
-            todo.done = e.target.checked;
-            localStorage.setItem('todos', JSON.stringify(todos));
+            todoList.appendChild(todoItem);
 
             if (todo.done) {
                 todoItem.classList.add('done');
-            } else {
-                todoItem.classList.remove('done');
             }
 
-            DisplayTodos();
-        })
+            input.addEventListener('click', e => {
+                todo.done = e.target.checked;
+                localStorage.setItem('todos', JSON.stringify(todos));
 
-        edit.addEventListener('click', e => {
-            const input = content.querySelector('input');
-            input.removeAttribute('readonly');
-            input.focus();
-            input.addEventListener('blur', e => {
-                input.setAttribute('readonly', true);
-                todo.content = e.target.value;
+                if (todo.done) {
+                    todoItem.classList.add('done');
+                } else {
+                    todoItem.classList.remove('done');
+                }
+
+                DisplayTodos();
+            })
+
+            edit.addEventListener('click', e => {
+                const input = content.querySelector('input');
+                input.removeAttribute('readonly');
+                input.focus();
+                input.addEventListener('blur', e => {
+                    input.setAttribute('readonly', true);
+                    todo.content = e.target.value;
+                    localStorage.setItem('todos', JSON.stringify(todos));
+                    DisplayTodos();
+                })
+            })
+
+            deleteButton.addEventListener('click', e => {
+                todos = todos.filter(t => t != todo);
                 localStorage.setItem('todos', JSON.stringify(todos));
                 DisplayTodos();
             })
         })
-
-        deleteButton.addEventListener('click', e => {
-            todos = todos.filter(t => t != todo);
-            localStorage.setItem('todos', JSON.stringify(todos));
-            DisplayTodos();
-        })
-    })
 }
 
 //CODE SNIPETS TO ADD ALERT SOUND IF TODO OVERDUE? (SEE READ ME ALSO - AFTER DATE/TIME ADDED - ALERT WHEN 20:00 REACHED ON DAY ADDED AND NOT TICKED AS DONE OR LINK TO RED/DANGER IN CSS ln 244 - 266 MAYBE?)
