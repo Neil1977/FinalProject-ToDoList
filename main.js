fc7c152913f5dc50b8c4bc82895decb0c6fca007
Neil1977 <Neil_lunn1977@hotmail.com>
2022-09-14 18:12:31 +0100

    updated with commented code time/date

--- main.js
+++ main.js
@@ -14,12 +14,32 @@ window.addEventListener('load', () => {
     newTodoForm.addEventListener('submit', e => {
         e.preventDefault();
 
-        //WANT TO USE BLOCK BELOW TO ADD DATE TO APP SCREEN
+        //WANT TO USE  CODE BLOCK BELOW TO ADD DATE TO APP SCREEN:
 
-        //const dateElement = document.getElementById('date');
-        //let options = { weekday:'long', month:'short', day:'numeric'};
-        //let today = new Date();
-        //dateElement.innerHTML = today.toLocaleDateString("en-UK", options);
+        //const timeElement = document.querySelector(".time");
+        //const dateElement = document.querySelector(".date");
+
+        //function formatTime(date) {
+        //const hours12 = date.getHours() % 12 || 12;
+        //const minutes = date.getMinutes();
+        //const isAm = date.getHours() < 12;
+
+        //return `${hours12.toString()};${minutes.toString().padStart(2, "0")} ${isAm ? "AM : "PM"}`;
+        //}
+
+        //function formatDate(date) {
+            //const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+            //const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+            //return `${days[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear}`;
+        //}
+
+        //setInterval(() => {
+            //const now = new Date();
+
+            //timeElement.textContent = formatTime(now);
+            //dateElement.textContent = formatDate(now);
+        //}, 200);
 
         const todo = {
             content: e.target.elements.content.value,
