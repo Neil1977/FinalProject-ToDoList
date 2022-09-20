84174bd3e680e61e45169dc801f9d46639845319
Neil1977 <Neil_lunn1977@hotmail.com>
2022-09-20 10:02:54 +0100

    updated

--- main.js
+++ main.js
@@ -70,14 +70,53 @@ setInterval(() => {
 //ln39 - function formatTime(date) { returns time only........
 
 //A BIT LIKE THIS???
-//function NewItem({ add, cancel }) {
-    //const [name, setName] = React.useState("");
-    //const [date, setDate] = React.useState(dateformat(new Date(), "yyyy-mm-dd"));
+/*function NewItem({ add, cancel }) {
+    const [name, setName] = React.useState("");
+    const [date, setDate] = React.useState(dateformat(new Date(), "yyyy-mm-dd"));
   
-    //function addItem() {
-      //const dueDate = new Date(date);
-      //add({ name, timestampDue: dueDate.getTime(), complete: false, id: 0 });
-    //}
+    function addItem() {
+      const dueDate = new Date(date);
+      add({ name, timestampDue: dueDate.getTime(), complete: false, id: 0 });
+    }
+  
+    return (
+      <div className="add-item-form">
+        <div className="form-group">
+          <label htmlFor="addItemInput">Item description</label>
+          <input
+            type="text"
+            placeholder="Enter description..."
+            className="form-control"
+            id="addItemInput"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="addItemDueInput">Due date</label>
+          <input
+            type="date"
+            className="form-control"
+            id="addItemDueInput"
+            onChange={e => setDate(e.target.value)}
+            value={date}
+          />
+        </div>
+        <button className="btn btn-success" disabled={name === ""} onClick={addItem}>
+          Add item
+        </button>
+        <button className="btn btn-secondary" style={{ marginLeft: "20px" }} onClick={cancel}>
+          Cancel
+        </button>
+      </div>);
+  }
+  const [adding, setAdding] = React.useState(false);
+  {!adding && (
+  <button type="button" className="btn btn-link" onClick={() => setAdding(true)}>
+    Add new item
+  </button>)}
+  {adding && <NewItem cancel={() => setAdding(false)} add={addNewItem} />}
+  */
 
 //TOGGLE NOW WORKS BUT REORDERS ENTRIES IN CHRONOLOGICAL BUT ALSO TO TOP/BOTTOM OF LIST AND IDEALLY JUST ORDER NOT LIST POSITION
 
