eebe72aa3f4af2f1f6155c8e44982c82503b81e3
Neil1977 <Neil_lunn1977@hotmail.com>
2022-09-21 09:59:50 +0100

    updated comments in code for explanation

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
+//Setting up local storage & retrieval below
 window.addEventListener('load', () => {
     todos = JSON.parse(localStorage.getItem('todos')) || [];
     const nameInput = document.querySelector('#name');
     const newTodoForm = document.querySelector('#new-todo-form');
-
     const username = localStorage.getItem('username') || '';
 
+    //setting item in local storage to save name (key)
     nameInput.value = username;
-
     nameInput.addEventListener('change', e => {
         localStorage.setItem('username', e.target.value);
     })
-
+    //Prevent default page reload
     newTodoForm.addEventListener('submit', e => {
         e.preventDefault();
 
+        //setting up categories and timestamp functionality
         const todo = {
             content: e.target.elements.content.value,
             category: e.target.elements.category.value,
@@ -22,54 +23,52 @@ window.addEventListener('load', () => {
             timeCreated: new Date().toLocaleTimeString("en-GB", { timeZone: 'Europe/London' }),
         }
 
+        //Add to todos array elements and changing to JSON string to store (only way in local storage - primitive values - no REACT/Vanilla JS)
         todos.push(todo);
         localStorage.setItem('todos', JSON.stringify(todos));
-
         e.target.reset();
 
+        //This function relocated below to incorporate more functionality.
         DisplayTodos();
     })
 
     DisplayTodos();
 })
 
+//Adding functionality of date and time on front of app
 const timeElement = document.querySelector(".time");
 const dateElement = document.querySelector(".date");
 
+//Setting up function to convert timestamp into usable display
 function formatTime(date) {
     const hours12 = date.getHours() % 12 || 12;
     const minutes = date.getMinutes();
     const isAm = date.getHours() < 12;
-
     return `${hours12.toString()}:${minutes.toString().padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
 }
 
+//Setting up function to convert timestamp into usable display
 function formatDate(date) {
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
     return `${days[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`;
 }
 
+//Allow real time update of calendar on front of app (will auto refresh every based on setting decrement below)
 setInterval(() => {
     const now = new Date();
-
     timeElement.textContent = formatTime(now);
     dateElement.textContent = formatDate(now);
 }, 200);
 
-
-
-
-
-
 const toggleDatetime = document.getElementById("toggleDatetime")
 toggleDatetime.addEventListener("change", (e) => DisplayTodos(e.target.checked));
+
+//Setting up function and all elements/labels and inputs to link to HTML/CSS
 function DisplayTodos(isAscending = false) {
     const todoList = document.querySelector('#todo-list');
     todoList.innerHTML = '';
     const sortOrder = (isAscending) ? (a, b) => b.createdAt - a.createdAt : (a, b) => a.createdAt - b.createdAt;
-
     todos.sort(sortOrder)
         .forEach(todo => {
             const todoItem = document.createElement('div');
@@ -85,17 +84,19 @@ function DisplayTodos(isAscending = false) {
             input.checked = todo.done;
             span.classList.add('bubble');
 
+            //Seperating todos into business & Personal
             if (todo.category == 'personal') {
                 span.classList.add('personal');
             } else {
                 span.classList.add('business');
             }
-
             content.classList.add('todo-content');
             actions.classList.add('actions');
             edit.classList.add('edit');
             deleteButton.classList.add('delete');
-            //This is the input - changed it to a textarea, which can handle multiple lines. - STILL TO RESOLVE - SEE README - EDGE BUG
+
+            //This is the input - changed it to a textarea, which can handle multiple lines to ty and display more on multiple lines. - STILL TO RESOLVE - SEE README - EDGE BUG!
+            //Also added timeCreated here to link to timestamp to add to to do item - time only
             content.innerHTML = `<div type="textarea" class="input-text"  contenteditable="false">
                                     ${todo.content}
                                     ${todo.timeCreated}
@@ -103,6 +104,7 @@ function DisplayTodos(isAscending = false) {
             edit.innerHTML = 'Edit';
             deleteButton.innerHTML = 'Delete';
 
+            //Appending elements below
             label.appendChild(input);
             label.appendChild(span);
             actions.appendChild(edit);
@@ -110,14 +112,13 @@ function DisplayTodos(isAscending = false) {
             todoItem.appendChild(label);
             todoItem.appendChild(content);
             todoItem.appendChild(actions);
-
             todoList.appendChild(todoItem);
 
             if (todo.done) {
                 todoItem.classList.add('done');
             }
 
-            //ADDITIONAL STRETCH - ADD ESTIMATED COMPLETION TIME ON INPUT SO ALERT BELOW DOESNT GENERICALLY TRIGGER @ 18:00?
+            //ADDITIONAL STRETCH - ADDED ALERT LINKED TO TIMESTAMP TO SOUND ALERT IF 18:00 RECAHED AND TODO ITEM LEFT INCOMPLETED
             const date = new Date();
             const time = date.getHours();
             if (time >= 18) {
